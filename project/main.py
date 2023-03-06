8b8b5ea0b772012092166a9d6aa83942f34abb2e
Matan <matanbryant@gmail.com>
2023-03-05 20:49:29 -0500

    Added login/register functions, restructured app factory, added navbar

---
+++ project/main.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    print(current_user.is_authenticated)
+    return render_template("home.html")
+
+@main.route('/profile')
+@login_required
+def profile():
+    
+    print(current_user)
+    return render_template("profile.html", name=current_user.name)
